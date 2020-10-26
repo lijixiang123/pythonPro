081363901025d23452c567c00f3f1f5181fe783c
lijixiang <5251197+ljxyyq@user.noreply.gitee.com>
2020-10-26 17:27:24 +0800

    1

---
+++ com/qtt/math/pack/amimal.py
@@ -0,0 +1,9 @@
+class Animal():
+
+    def __init__(self,name,sex):
+        self.name = name
+        self.sex = sex
+
+class Cat(Animal):
+    def typ(self):
+        return '哺乳动物'
---
+++ com/qtt/math/test2.py
@@ -0,0 +1,15 @@
+
+from pack.amimal import Animal
+from pack.amimal import Cat
+
+ani = Animal('二哈','公')
+
+print(ani.name)
+print(ani.sex)
+
+print("------------")
+
+Acat = Cat('小咪咪','母')
+print(Acat.name)
+print(Acat.sex)
+print(Acat.typ())
